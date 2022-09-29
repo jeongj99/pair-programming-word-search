a589b2ca90bfcfc27c6ea2afb27ec8eee879fe7b
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-29 04:00:38 +0000

    Created test cases for wordsearch. Still missing the diagonal search part

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,9 +1,11 @@
+const { expect } = require('chai');
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
+
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
@@ -15,12 +17,28 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
     assert.isFalse(result);
   });
 
-  it("should return true if the word is present", function() {
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'H', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'A', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'R', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'L', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'I', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'M', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'A', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'N', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHARLIMAN');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
@@ -31,8 +49,74 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
+
+  });
+
+  it("should return true if the word is present vertically and backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'N', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'A', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'M', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'I', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'L', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'A', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'H', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'C', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'CHARLIMAN');
+
+    assert.isTrue(result);
+  });
+
+  it("should return true if the word is present horizontally and backwards", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'SEINFELD');
+
+    assert.isTrue(result);
+
+  });
+  it("returns false if word is an empty string", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], '');
+
+    assert.isFalse(result);
+
+  });
+
+  it("should throw an error if the word passed is not a string", function() {
+    const letters = [
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'L', 'E', 'F', 'N', 'I', 'E', 'S'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ];
+    const word = undefined;
+    expect(wordSearch, (letters, word)).to.throw("Pass a string for word");
   });
 });
