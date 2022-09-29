069a14f6e03427a2339c74f4c1645797ec88eaa1
Jae Yun Jeong <jeongalex0616@gmail.com>
2022-09-29 03:58:17 +0000

    Created the wordsearch function. Still missing the part to search diagonally

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,27 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  if (typeof word !== 'string') {
+    throw new Error('Pass a string for word.');
+  }
+  if (word === "") {
+    return false;
+  }
+  const transposedLetters = letters[0].map((_, columnIndex) => letters.map(row => row[columnIndex]));
+  const verticalJoin = transposedLetters.map(el => el.join(''));
+  const horizontalJoin = letters.map(ls => ls.join(''));
 
-module.exports = wordSearch
+  for (const v of verticalJoin) {
+    let reverseV = v.split("").reverse().join("");
+    if (v.includes(word) ||
+      reverseV.includes(word)) return true;
+  }
+
+  for (const h of horizontalJoin) {
+    let reverseH = h.split("").reverse().join("");
+    if (h.includes(word) ||
+      reverseH.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
